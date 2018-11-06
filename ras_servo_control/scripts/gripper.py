6fcb247bab7ed4e19b97201cd6b80b960a72be82
Landzs <landzs@hotmail.com>
2018-11-06 11:07:11 +0100

    Update Gripper

--- ras_servo_control/scripts/gripper.py
+++ ras_servo_control/scripts/gripper.py
@@ -15,35 +15,54 @@ FLAG_OPEN = False
 FLAG_GRIP = False
 
 
+# #####################################################
+# #                    flag callback                  #
+# #####################################################
+# def flag_callback(msg):
+#     global FLAG_GRIPPED, FLAG_FOUND_OBJECT,FLAG_CLOSE, FLAG_GRIP, FLAG_OPEN
+#     flag = msg.data
+#     #rospy.loginfo('flag')
+#     if flag == "detect_object_done" and not FLAG_FOUND_OBJECT: 
+#         FLAG_OPEN = True
+#         FLAG_FOUND_OBJECT = True
+#     elif flag == "path_following_done":
+#         if  FLAG_FOUND_OBJECT:
+#             FLAG_GRIP = True
+#             FLAG_GRIPPED = True
+#         elif FLAG_GRIPPED:
+#             FLAG_OPEN = True
+#             FLAG_FOUND_OBJECT = False
+#             FLAG_GRIPPED = False
+#         else:
+#             pass
+#     else:
+#         pass
+
 #####################################################
-#                    flag callback                  #
+#                 gripper callback                  #
 #####################################################
-def flag_callback(msg):
+def grip_callback(msg):
     global FLAG_GRIPPED, FLAG_FOUND_OBJECT,FLAG_CLOSE, FLAG_GRIP, FLAG_OPEN
     flag = msg.data
     #rospy.loginfo('flag')
-    if flag == "detect_object_done" and not FLAG_FOUND_OBJECT: 
+    if flag == "open" : 
         FLAG_OPEN = True
-        FLAG_FOUND_OBJECT = True
-    elif flag == "path_following_done":
-        if  FLAG_FOUND_OBJECT:
-            FLAG_GRIP = True
-            FLAG_GRIPPED = True
-        elif FLAG_GRIPPED:
-            FLAG_OPEN = True
-            FLAG_FOUND_OBJECT = False
-            FLAG_GRIPPED = False
-        else:
-            pass
-    else:
-        pass
+    elif flag == "close":
+        FLAG_CLOSE = True
+    elif flag = "grip":
+        FLAG_GRIP = True
+
+
+
 
 def main():
     global FLAG_GRIPPED, FLAG_FOUND_OBJECT, FLAG_CLOSE, FLAG_GRIP, FLAG_OPEN
     rospy.init_node('gripper_node')
     GRIP = rospy.ServiceProxy('/arduino_servo_control/set_servo_angles', SetServoAngles)
     time.sleep(2)
-    rospy.Subscriber("/flag_done", std_msgs.msg.String, flag_callback)
+    # rospy.Subscriber("/flag_done", std_msgs.msg.String, flag_callback)
+    rospy.Subscriber("/gripper_state", std_msgs.msg.String, grip_callback)
+
     while not rospy.is_shutdown():
         if FLAG_OPEN:
             GRIP(70, 110) 
